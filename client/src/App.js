13f874f6726c2a416218e2f5bc04b58c3a39df02
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2026-05-26 11:05:03 -0700

    polished ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import "./index.css";
 
 function App() {
-
   const [wedding, setWedding] = useState(null);
 
   useEffect(() => {
@@ -12,35 +11,33 @@ function App() {
   }, []);
 
   if (!wedding) {
-    return <h2>Loading...</h2>;
+    return <h2>Loading wedding data...</h2>;
   }
 
   return (
     <div className="container">
-
       <h1>{wedding.coupleNames}</h1>
-
       <p>
         <strong>Date:</strong> {wedding.weddingDate}
       </p>
-
       <p>
         <strong>City:</strong> {wedding.city}
       </p>
-
       <h2>Wedding Events</h2>
-
-      <ul>
+      <ul className="event-list">
         {wedding.events.map((event, index) => (
           <li key={index}>
             <strong>{event.name}</strong>
             <div>{event.location}</div>
             <div>{event.address}</div>
-            <div>{event.startTime}</div>
+            <div>{new Date(event.startTime).toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit"
+            })}</div>
+            <br></br>
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
